da0c9325e48f75f6194e9fd9470d251b3032307c
Mateusz-Kubikowski <mateusz.kubikowski@o2.pl>
2025-03-27 13:18:26 +0100

    Create Find AD computer that excluded from group.ps1

---
+++ Find AD computer that excluded from group/Find AD computer that excluded from group.ps1
@@ -0,0 +1,27 @@
+Import-Module ActiveDirectory
+
+#Variables
+$OU = "OU=Servers,DC=Contoso,DC=com"
+$GroupName = "Domain computers"
+$PathToSave =  "C:\temp\"
+
+# Get the distinguished name of the group
+$Group = Get-ADGroup -Identity $GroupName -ErrorAction Stop
+
+# Get all computer accounts in the specified OU
+$Computers = Get-ADComputer -SearchBase $OU -Filter *
+
+# Loop through each computer and check group membership
+foreach ($Computer in $Computers) {
+    $ComputerName = $Computer.SamAccountName
+    
+    # Check if the computer is a member of the group
+    $IsMember = Get-ADGroupMember -Identity $GroupName -Recursive |
+                 Where-Object { $_.ObjectClass -eq 'computer' -and $_.SamAccountName -eq $ComputerName }
+
+    if ($IsMember) {
+        "$ComputerName is a member of $GroupName" >> $PathToSave\IsMember$groupname.txt
+    } else {
+        "$ComputerName is NOT a member of $GroupName" >> $PathToSave\IsNotMember$groupname.txt
+    }
+}

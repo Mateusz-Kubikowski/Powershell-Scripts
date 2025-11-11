90345b743a21744dc0c7c38ee05ca447ff081ddc
Mateusz-Kubikowski <mateusz.kubikowski@o2.pl>
2025-11-11 13:53:07 +0100

    Fix global variable assignment and add newline
    Add newline at the end of the file and update global variable assignment.

---
+++ Count_if_only_1_object_ivanti/Count_if_only_1_object.ps1
@@ -0,0 +1,19 @@
+$global:hostname_intrumssodev =([System.Net.Dns]::GetHostByName($env:computerName)).HostName| ConvertTo-Json
+$a = "error"
+$a = (Get-ADUser -Filter *).Count
+$global:users = "$a"
+
+$a = "error"
+$a = (Get-ADGroup -Filter *).Count
+$global:groups = "$a"
+
+$a = "error"
+$a = (Get-ADComputer -Filter *) | Measure-Object |select count -ExpandProperty count
+$global:computers = "$a"
+
+$a = "error"
+$a = (Get-ADDomainController -Filter *) | Measure-Object |select count -ExpandProperty count
+$global:DomainC = "$a"
+
+
+#If only one object use measure-object, in other case count is enought

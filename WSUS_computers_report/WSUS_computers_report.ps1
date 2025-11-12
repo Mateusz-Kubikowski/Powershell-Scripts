a7a4a3a3ff82a68b7d45aa822ade0cead7b15ac1
Mateusz-Kubikowski <mateusz.kubikowski@o2.pl>
2025-11-12 17:22:01 +0100

    Add WSUS computers report PowerShell script

---
+++ WSUS_computers_report/WSUS_computers_report.ps1
@@ -0,0 +1,34 @@
+# Connect to the WSUS server
+$domain = Get-WmiObject -Namespace root\cimv2 -Class Win32_ComputerSystem | select domain -ExpandProperty domain
+$wsusServer = Get-WsusServer -Name "$env:COMPUTERNAME.$domain" -Port 8531 -UseSsl
+$date = get-date -Format dd_MM_yyyy
+$FilePath = "c:\temp\"+$domain+"_WSUS_report_"+$date+".csv"
+
+$wsusgroups = $wsusServer.GetComputerTargetGroups() | select name -ExpandProperty name
+$wsusgroups = $wsusgroups | Where-Object {$_ -ne 'All Computers'}
+
+$table = @()
+# Iterate over each group and list the servers
+foreach ($wsusgroup in $wsusgroups) {
+    # Get the target group
+    $group = $wsusServer.GetComputerTargetGroups() | Where-Object { $_.Name -eq $wsusgroup }
+    
+    if ($group -ne $null) {
+        
+        # Get all computers in the group
+        $computers = $group.GetComputerTargets()
+        
+        foreach ($computer in $computers) {
+            $comp = $computer.FullDomainName
+            $table += [PSCustomObject]@{
+            Hostname = $comp
+            Groupname = $wsusgroup
+            }
+        }
+
+
+    } else {
+        Write-Output "Group '$wsusgroup' not found!"
+    }
+}
+$table | export-csv -Path $FilePath -NoTypeInformation

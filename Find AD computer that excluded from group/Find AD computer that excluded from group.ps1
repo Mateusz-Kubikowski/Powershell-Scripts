60fbb9c5cac6eb5c0a5e91473119aa652e854128
Mateusz-Kubikowski <mateusz.kubikowski@o2.pl>
2025-11-21 12:04:44 +0100

    Update Find AD computer that excluded from group.ps1

--- Find AD computer that excluded from group/Find AD computer that excluded from group.ps1
+++ Find AD computer that excluded from group/Find AD computer that excluded from group.ps1
@@ -11,13 +11,14 @@ $Group = Get-ADGroup -Identity $GroupName -ErrorAction Stop
 # Get all computer accounts in the specified OU
 $Computers = Get-ADComputer -SearchBase $OU -Filter *
 
+$GroupMembers = Get-ADGroupMember -Identity $GroupName -Recursive
+
 # Loop through each computer and check group membership
 foreach ($Computer in $Computers) {
     $ComputerName = $Computer.SamAccountName
     
     # Check if the computer is a member of the group
-    $IsMember = Get-ADGroupMember -Identity $GroupName -Recursive |
-                 Where-Object { $_.ObjectClass -eq 'computer' -and $_.SamAccountName -eq $ComputerName }
+    $IsMember = $GroupMembers | Where-Object { $_.ObjectClass -eq 'computer' -and $_.SamAccountName -eq $ComputerName }
 
     if ($IsMember) {
         "$ComputerName is a member of $GroupName" >> $PathToSave\IsMember$groupname.txt
